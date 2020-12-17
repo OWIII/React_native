bf4cc71ab5015178ca66357c9cc791bedf8cbbfb
Lusnikov.A <Lusnikov.A@digdes.com>
2020-12-17 03:11:02 +0300

    init project

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello world!</Text>
     </View>
   );
 }
@@ -18,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#000',
+    fontSize: 26,
+  }
 });
